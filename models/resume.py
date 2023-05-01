9385cead0b4dfd282ebbfead7ef7c154bfc31fe2
MarioAlessandroNapoli2 <marioalessandro.napoli@expleoitalia.com>
2023-05-01 13:17:02 +0200

    Added langdetect, compressed init, added 'companies_worked_for' and 'total_work_experience' fields

--- models/resume.py
+++ models/resume.py
@@ -1,19 +1,15 @@
 import os
 from typing import Optional
-from utils import file_to_text, detect_language, supported_formats
+from langdetect import detect
+from utils import file_to_text, supported_formats
 
 
 class Resume:
     def __init__(self, file_path: str):
         self.file_path = file_path
         self.file_extension = os.path.splitext(file_path)[1].lower()
-        self.language = None
-        self.raw_text = None
-        self.contact_info = {}
-        self.education = []
-        self.experience = []
-        self.skills = []
-        self.summary = None
+        self.skills, self.experience, self.education, self.contact_info,\
+            self.language, self.raw_text, self.summary, self.companies, self.total_work_experience = None
 
         if self.file_extension not in supported_formats:
             raise ValueError(f"Unsupported file format: {self.file_extension}")
@@ -25,13 +21,15 @@ class Resume:
         self.raw_text = file_to_text(self.file_path, self.file_extension)
 
     def _detect_language(self):
-        self.language = detect_language(self.raw_text)
+        self.language = detect(self.raw_text)
 
-    def process(self, summary_length: Optional[int] = None):
+    def process(self, summary_length: Optional[int] = 500):
         self._extract_contact_info()
         self._extract_education()
         self._extract_experience()
         self._extract_skills()
+        self._extract_companies()
+        self._extract_total_work_experience()
         self._generate_summary(summary_length)
 
     def _extract_contact_info(self):
@@ -50,7 +48,15 @@ class Resume:
         # Implement skills extraction logic based on self.language and self.raw_text
         return
 
-    def _generate_summary(self, summary_length: Optional[int] = None):
+    def _extract_companies(self):
+        # Implement skills extraction logic based on self.language and self.raw_text
+        return
+
+    def _extract_total_work_experience(self):
+        # Implement skills extraction logic based on self.language and self.raw_text
+        return
+
+    def _generate_summary(self, summary_length):
         # Implement summary generation logic using ChatGPT API
         return
 
@@ -62,5 +68,7 @@ class Resume:
             "education": self.education,
             "experience": self.experience,
             "skills": self.skills,
+            "companies_worked_for": self.companies,
+            "total_work_experience": self.total_work_experience,
             "summary": self.summary,
         }
