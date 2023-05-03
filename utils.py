c9a22d5579755b8b065f6ff15afa1e5f95cc530c
MarioAlessandroNapoli2 <marioalessandro.napoli@expleoitalia.com>
2023-05-04 00:22:00 +0200

    Added openai key, to link from somewhere else. Deleted not used function. Completed GPT flow with a main prompt to response function, some utilities to generate prompts messages, and response skeleton for CV filling.

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import json
 import openai
 from docx2pdf import convert
@@ -7,6 +6,7 @@ from typing import List, Dict
 from pdfminer.high_level import extract_text
 
 supported_formats = ('.pdf', '.docx', '.txt')
+openai.api_key = '##'
 
 
 def get_file_list(folder_path: str) -> List[str]:
@@ -25,37 +25,8 @@ def get_file_list(folder_path: str) -> List[str]:
     return file_list
 
 
-def export_data_to_file(data: List[Dict], file_format: str, output_folder: str) -> None:
-    """
-    Exports the extracted resume data to a file in the specified format (CSV or JSON).
-
-    :param data: A list of dictionaries containing the extracted resume data.
-    :param file_format: The output file format (CSV or JSON).
-    :param output_folder: The folder to save the exported file.
-    """
-    if file_format.lower() == "csv":
-        export_to_csv(data, output_folder)
-    elif file_format.lower() == "json":
-        export_to_json(data, output_folder)
-    else:
-        raise ValueError(f"Unsupported file format: {file_format}")
-
-
-def export_to_csv(data: List[Dict], output_folder: str) -> None:
-    output_file = os.path.join(output_folder, "resume_data.csv")
-
-    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = data[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
-
-
 def export_to_json(data: List[Dict], output_folder: str) -> None:
     output_file = os.path.join(output_folder, "resume_data.json")
-
     with open(output_file, 'w', encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, ensure_ascii=False, indent=4)
 
@@ -72,16 +43,134 @@ def file_to_text(file_path: str, file_extension: str) -> str:
         text = extract_text(file_path)
     elif file_extension.lower() == ".docx":
         convert(file_path)
-        new_file_path = os.path.splitext(file_path)[0]+'.pdf'
+        new_file_path = os.path.splitext(file_path)[0] + '.pdf'
         text = extract_text(new_file_path)
     else:
         raise ValueError(f"Unsupported file format: {file_extension}")
     return text
 
 
-def generate_text(messages: List[Dict[str, str]]) -> str:
-    response = openai.ChatCompletion.create(
+def get_gpt_response(messages, temp=0.4):
+    res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages
+        messages=messages,
+        temperature=temp,
+        stream=True
     )
-    return response['choices'][0]['message']['content']
+    res_len = 0
+    total_message = ""
+    for chunk in res:
+        chunk_message = chunk['choices'][0]['delta']  # extract the message
+        if 'content' in chunk_message.keys():
+            message = chunk_message['content']
+            print(f"{message}", end="")
+            res_len += len(message)
+            total_message += message
+    return total_message
+
+
+def generate_json_response(cv) -> dict:
+    cv_message = get_cv_extracion_messages(cv)
+    print('------------\nCV\n------------\n')
+    json_output = get_gpt_response(cv_message)
+    try:
+        parsed_json = json.loads(json_output)
+        return parsed_json
+    except json.JSONDecodeError as error:
+        correction_messages = get_json_correction_messages(error, json_output)
+        corrected_json_output = get_gpt_response(correction_messages)
+        try:
+            print('------------\nCORRECTING\n------------\n')
+            parsed_json = json.loads(corrected_json_output)
+            return parsed_json
+        except json.JSONDecodeError as e:
+            print(f"Cannot produce output. Error: {e}")
+
+
+def get_cv_extracion_messages(txt):
+    messages = [
+        {"role": "system",
+         "content": "You are a CV info extractor backend API, you can output only JSON results without any words or explanation, just like an API"},
+        {"role": "user", "content": f"I will give you a cv in any possible language and a skeleton for a JSON output you have to fill.\
+         \n\n1)Summarize all the job and education description you write to a maximum of 200 chars.\n2)Follow the skeleton schema and output as many information you can find.\
+         \n\nCV:{txt}\n\nSKELETON:{skeleton}\n\nJSON RESULT:\n"}
+    ]
+    return messages
+
+
+def get_json_correction_messages(error, corrupted_json):
+    messages = [
+        {"role": "system",
+         "content": "You are a JSON CORRECTION API, you can output only JSON results without any words or explanation, just like an API"},
+        {"role": "user",
+         "content": f"When I try to load this JSON with Python json.loads(json_txt) I receive this error message: {error.msg}.\nCorrect the following JSON to a valid JSON that can be loaded.\n\nJSON:{corrupted_json}"}
+    ]
+    return messages
+
+
+def get_summary_messages(cv, summary_length=500):
+    messages = [
+        {"role": "system", "content": "You are a CV summarization tool"},
+        {"role": "user", "content": f"I will give you a cv in any possible language, you have to summarize it in {summary_length} chars.\
+         \n\nCV:{cv}"}
+    ]
+    return messages
+
+
+def get_gpt_summary(cv, summary_length):
+    summary_prompt = get_summary_messages(cv, summary_length)
+    print('------------\nSUMMARY\n------------\n')
+    summary_txt = get_gpt_response(summary_prompt, 1)
+    return summary_txt
+
+
+skeleton = """
+{
+    contact_info:{
+        name:str,
+        location:str,
+        address:str,
+        phone:str,
+        email:str,
+    }
+    work_experience:[
+        {
+            title:str,
+            company:str,
+            location:str,
+            start:date,
+            end:date,
+            description(summary in 200 chars):str
+        },
+        ...,
+        ...
+    ],
+    education:[
+        {
+            degree:str,
+            institution:str,
+            location:str,
+            start:str,
+            end:str,
+            description(summary in 200 chars):str
+            grade:str,
+            thesis_title:str
+        },
+        ...,
+        ...
+    ]
+    skills:{
+        category:[skills],
+        category:[skills],
+        ...
+    }
+    companies_worked_for:[
+        {
+            name:str,
+            location:str
+        },
+        ...,
+        ...
+    ]
+}
+"""
